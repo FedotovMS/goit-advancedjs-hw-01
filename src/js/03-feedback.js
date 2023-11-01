9d1035ee8366c924986f535d5cd01c4e6cff5261
Mykola Fedotov <fedotovns@gmail.com>
2023-11-01 14:39:12 +0100

    fixed some issues

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,40 +1,41 @@
 import throttle from 'lodash.throttle';
 
-const refs = {
-  form: document.querySelector('.feedback-form'),
+const form = document.querySelector('.feedback-form');
+const LS_KEY = 'feedback-form-state';
+
+let formData = {
+  email: '',
+  message: '',
 };
 
-refs.form.addEventListener('input', onChange);
-refs.form.addEventListener('submit', onSubmit);
-
-const thtottledLocalSorageUpdate = throttle(inputData => {
-  localStorage.setItem('feedback-form-state', JSON.stringify(inputData));
-}, 500);
-
-function onChange(e) {
-  const inputData = {
-    email: refs.form.elements.email.value.trim(),
-    message: refs.form.elements.message.value.trim(),
-  };
-
-  thtottledLocalSorageUpdate(inputData);
+function resetFormData() {
+  formData = { email: '', message: '' };
 }
 
-const dataFromStorage =
-  JSON.parse(localStorage.getItem('feedback-form-state')) || {};
-
-if (dataFromStorage.email) {
-  refs.form.elements.email.value = dataFromStorage.email;
-}
-if (dataFromStorage.message) {
-  refs.form.elements.message.value = dataFromStorage.message;
+resetFormData();
+if (localStorage.getItem(LS_KEY)) {
+  formData = JSON.parse(localStorage.getItem(LS_KEY));
 }
 
-function onSubmit(e) {
+form.elements.email.value = formData.email;
+form.elements.message.value = formData.message;
+
+form.addEventListener(
+  'input',
+  throttle(e => {
+    formData[e.target.name] = e.target.value;
+    localStorage.setItem(LS_KEY, JSON.stringify(formData));
+  }, 500)
+);
+
+form.addEventListener('submit', e => {
+  if (!formData.message || !formData.email) {
+    alert('you have to fill in all fields');
+    return;
+  }
   e.preventDefault();
-
-  dataFromStorage ?? console.log(dataFromStorage);
-
-  localStorage.removeItem('feedback-form-state');
-  refs.form.reset();
-}
+  console.log(formData);
+  resetFormData();
+  localStorage.removeItem(LS_KEY);
+  form.reset();
+});
