326958272ad0a54528d83e278ee7b5bd43e7226a
Mykola Fedotov <fedotovns@gmail.com>
2023-11-01 14:16:56 +0100

    task02 - fixed playedTime checkout

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -3,9 +3,14 @@ import throttle from 'lodash.throttle';
 
 const iframe = document.querySelector('#vimeo-player');
 const player = new Player(iframe);
+const LS_KEY = 'videoplayer-current-time';
+
+const playedTime = localStorage.getItem(LS_KEY) ?? 0;
+
+player.setCurrentTime(playedTime);
 
 const throttledLocalStorageUpdate = throttle(seconds => {
-  localStorage.setItem('videoplayer-current-time', seconds);
+  localStorage.setItem(LS_KEY, Math.round(seconds));
 }, 1000);
 
 const onPlay = data => {
@@ -13,20 +18,3 @@ const onPlay = data => {
 };
 
 player.on('timeupdate', onPlay);
-
-player
-  .setCurrentTime(localStorage.getItem('videoplayer-current-time'))
-  .then(function (seconds) {
-    // seconds = the actual time that the player seeked to
-  })
-  .catch(function (error) {
-    switch (error.name) {
-      case 'RangeError':
-        // the time was less than 0 or greater than the video’s duration
-        break;
-
-      default:
-        // some other error occurred
-        break;
-    }
-  });
